438e5486367ab3d7421ce67852c7e1126cc33aca
Igor Radovanov <igor.radovanov@outlook.com>
2024-11-10 23:54:46 +0100

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+import Globe from "globe.gl";
+
+function App() {
+  const globeEl = useRef();
+
+  useEffect(() => {
+    // Import local file globe_data.json
+    import("../globe_data.json").then((data) => {
+      const globe = Globe()(globeEl.current)
+        .globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg")
+        .arcsData(data.default) // Use the imported data here
+        .arcColor("color")
+        .arcDashLength(0.4)
+        .arcDashGap(0.2)
+        .arcDashInitialGap(() => Math.random())
+        .arcDashAnimateTime(5000);
+    });
+
+    return () => {
+      if (globeEl.current) {
+        globeEl.current.innerHTML = "";
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <div ref={globeEl}></div>
+    </>
+  );
+}
+
+export default App;
